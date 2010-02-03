55d06f231caace8f864d0d1844cbca143dd4dbc1
Xiao <xiao@xiao-yu.com>
2010-02-03 16:35:29 -0500

    adding stuff

--- src/org/mcgill/telecom/Gateway.java
+++ src/org/mcgill/telecom/Gateway.java
@@ -1,5 +1,23 @@
 package org.mcgill.telecom;
+import java.io.IOException;
+import java.net.*;
 
 public class Gateway {
-
+	ServerSocket server;
+	Socket clientConnection;
+	
+	private void listen(){
+		try{
+			server = new ServerSocket(1337);
+			System.out.println("Waiting for connection ...");
+			clientConnection = server.accept();
+		}
+		catch(IOException ex){
+			
+		}		
+	}
+	
+	public void main(String args[]){
+		listen();
+	}
 }
