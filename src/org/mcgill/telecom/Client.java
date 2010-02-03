46e8af7f59457b6c065ca98dbdb6031bab95d837
Xiao <xiao@xiao-yu.com>
2010-02-03 17:49:53 -0500

    comm code

--- src/org/mcgill/telecom/Client.java
+++ src/org/mcgill/telecom/Client.java
@@ -1,5 +1,50 @@
 package org.mcgill.telecom;
+import java.net.*;
+import java.io.*;
 
 public class Client {
-
+	Socket serverConnection;
+	ObjectOutputStream send;
+	ObjectInputStream receive;
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	
+	private void connect(){
+		try{
+			serverConnection = new Socket("localhost", 1337);
+			System.out.println("Connected to gateway");
+			send = new ObjectOutputStream(serverConnection.getOutputStream());
+			send.flush();
+			receive = new ObjectInputStream(serverConnection.getInputStream());
+			
+			String username = ask("Username:");
+			String password = ask("Password:");
+			
+			sendMessage(new String[] {username, password});
+		}
+		catch(IOException ex){}
+	}
+	
+	private String ask(String query){
+		System.out.println(query);
+		try{
+			return reader.readLine();
+		}
+		catch(IOException ex){
+			return "";
+		}
+	}
+	
+	private void sendMessage(Object message){
+		try{
+			send.writeObject(message);
+			send.flush();
+		}
+		catch(IOException ex){
+			ex.printStackTrace();
+		}
+	}
+	
+	public void main(){
+		connect();
+	}
 }
--- src/org/mcgill/telecom/Gateway.java
+++ src/org/mcgill/telecom/Gateway.java
@@ -1,16 +1,29 @@
 package org.mcgill.telecom;
-import java.io.IOException;
 import java.net.*;
+import java.io.*;
 
 public class Gateway {
 	ServerSocket server;
 	Socket clientConnection;
+	ObjectOutputStream send;
+	ObjectInputStream receive;
 	
 	private void listen(){
 		try{
 			server = new ServerSocket(1337);
 			System.out.println("Waiting for connection ...");
 			clientConnection = server.accept();
+			System.out.println(clientConnection.getInetAddress().getHostName() + " has connected");
+			send = new ObjectOutputStream(clientConnection.getOutputStream());
+			send.flush();
+			receive = new ObjectInputStream(clientConnection.getInputStream());
+			
+			try {
+				String[] login = (String[]) receive.readObject();
+				System.out.print(login[0]);
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
 		}
 		catch(IOException ex){
 			
